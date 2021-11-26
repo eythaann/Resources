9eaa1b4b0855de7c3d4a4a5c91505fe929a1528d
Eythan David <eythan.cvt@gmail.com>
2021-11-26 10:38:54 -0500

    new files

---
+++ MissNumbers.js
@@ -0,0 +1,29 @@
+const arr = [1, 6, 5, 3, 4, 10];
+
+const FindNumbers = (arr, n) => {
+  const missNumbers = [];
+  arr.sort((a, b) => a - b);
+
+  if (arr[0] < 1) {
+    return new Error("The Array has a number smaller that " + 1);
+  }
+  if (arr[arr.length - 1] > n) {
+    return new Error("The Array has a number bigger that " + n);
+  }
+  if (arr[arr.length - 1] !== n) {
+    arr.push(n), missNumbers.push(n);
+  }
+
+  let expected = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (expected !== arr[i]) {
+      missNumbers.push(expected);
+      i--;
+    }
+    expected++;
+  }
+
+  return missNumbers;
+};
+
+console.log(FindNumbers(arr, 15));
